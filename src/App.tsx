72e6b0715fecc933d3c270cb17ff09f2313ad824
ErickAguilarGomez <lemayor01@hotmail.com>
2024-10-23 17:55:31 -0500

    fixed favicon

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ import Tecnologies from './components/Tecnologies';
 import Attestation from './components/Attestation';
 import { useEffect } from 'react';
 function App() {
-    
+
     useEffect(() => {
         const handleVisibilityChange = () => {
-            const favicon = document.querySelector("link[rel*='icon']");
+            const favicon = document.querySelector("link[rel*='icon']") as HTMLLinkElement
+
             if (document.visibilityState === 'visible') {
                 document.title = "Erick Aguilar | Portafolio";
-                 favicon.href = 'https://img.icons8.com/?size=100&id=GBu1KXnCZZ8j&format=png&color=000000'; // URL del favicon normal
+                favicon.href = 'https://img.icons8.com/?size=100&id=GBu1KXnCZZ8j&format=png&color=000000';
             } else {
                 document.title = 'Contrátame!';
-                favicon.href = 'https://img.icons8.com/?size=100&id=Ky6Dbm1oUuyh&format=png&color=000000'; // URL del favicon corazón roto
+                favicon.href = 'https://img.icons8.com/?size=100&id=Ky6Dbm1oUuyh&format=png&color=000000';
             }
-        };
+        }
+
         document.addEventListener('visibilitychange', handleVisibilityChange);
+        
         return () => {
             document.removeEventListener('visibilitychange', handleVisibilityChange);
         };
